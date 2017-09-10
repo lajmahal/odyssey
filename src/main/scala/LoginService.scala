6fa672095565a9193956eb3ef42f9e509bcf219e
Laj Lal <laj.lal@outlook.com>
2017-09-10 23:21:49 +0800

    Make LoginService maintain state of logged in users, and provide a way to query for the set of active users.

--- src/main/scala/LoginService.scala
+++ src/main/scala/LoginService.scala
@@ -1,4 +1,4 @@
-import LoginService.{RequestLogin, UserLoggedIn}
+import LoginService.{ReplyActiveUserList, RequestActiveUserList, RequestLogin, UserLoggedIn}
 import akka.actor.{Actor, ActorLogging, Props}
 
 object LoginService {
@@ -6,13 +6,22 @@ object LoginService {
 
   final case class RequestLogin(userId: String)
   final case class UserLoggedIn(userId: String)
+
+  case object RequestActiveUserList
+  final case class ReplyActiveUserList(users: Set[String])
 }
 
 class LoginService extends Actor with ActorLogging {
+  private var loggedInUsers: Set[String] = Set.empty[String]
+
   override def preStart(): Unit = log.info("LoginService started")
   override def postStop(): Unit = log.info("LoginService stopped")
 
   override def receive: Receive = {
-    case RequestLogin(userId) => sender() ! UserLoggedIn(userId)
+    case RequestLogin(userId) =>
+      loggedInUsers += userId
+      sender() ! UserLoggedIn(userId)
+
+    case RequestActiveUserList => sender() ! ReplyActiveUserList(loggedInUsers)
   }
 }
--- src/test/scala/LoginServiceUnitTest.scala
+++ src/test/scala/LoginServiceUnitTest.scala
@@ -18,5 +18,33 @@ class LoginServiceUnitTest(_system: ActorSystem) extends TestKit(_system)
       probe.expectMsg(LoginService.UserLoggedIn("user"))
       probe.lastSender shouldBe loginServiceActor
     }
+
+    "should reply to active user list requests" - {
+      "with an empty Set if no one logged in" in {
+        val probe = TestProbe()
+        val loginServiceActor = system.actorOf(LoginService.props())
+
+        loginServiceActor.tell(LoginService.RequestActiveUserList, probe.ref)
+        probe.expectMsg(LoginService.ReplyActiveUserList(Set.empty[String]))
+        probe.lastSender shouldBe loginServiceActor
+      }
+
+      "with users if some are logged in" in {
+        val probe = TestProbe()
+        val loginServiceActor = system.actorOf(LoginService.props())
+
+        loginServiceActor.tell(LoginService.RequestLogin("user1"), probe.ref)
+        probe.expectMsg(LoginService.UserLoggedIn("user1"))
+        probe.lastSender shouldBe loginServiceActor
+
+        loginServiceActor.tell(LoginService.RequestLogin("user2"), probe.ref)
+        probe.expectMsg(LoginService.UserLoggedIn("user2"))
+        probe.lastSender shouldBe loginServiceActor
+
+        loginServiceActor.tell(LoginService.RequestActiveUserList, probe.ref)
+        probe.expectMsg(LoginService.ReplyActiveUserList(Set("user1", "user2")))
+        probe.lastSender shouldBe loginServiceActor
+      }
+    }
   }
 }
